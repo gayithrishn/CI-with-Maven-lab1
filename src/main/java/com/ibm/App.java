0360287234096467b73c09bd35b5d8e3b7a4d755
gayithrishn <gayithrishn@gmail.com>
2025-03-24 18:02:36 +0530

    first updated code

---
+++ src/main/java/com/ibm/App.java
@@ -0,0 +1,17 @@
+package com.ibm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        for( int i=1;i<=10;i++)
+        {
+            System.out.println(" i =" + i);
+        }
+    }
+}
